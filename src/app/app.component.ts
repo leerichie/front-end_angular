b17b6036e1325585dffb7c7acee0883646eab667
leerichie <leerichie@wp.pl>
2022-10-16 16:41:34 +0200

    first angular site nes starting

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { User } from './user';
+import { UserService } from './user.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'user_manager_app';
+export class AppComponent implements OnInit{
+  public users: User[] | undefined;
+
+constructor(private userService: UserService){}
+
+ngOnInit(){
+    this.getUsers();
+}
+
+public getUsers(): void{
+  this.userService.getUsers().subscribe(
+    (response: User[]) => {
+this.users = response;
+    },
+    (error: HttpErrorResponse) => {
+      alert(error.message);
+    }
+    );
+  }
 }
---
+++ src/app/user.ts
@@ -0,0 +1,10 @@
+export interface User {
+    id: number;
+    name: string;
+    email: string;
+    password: string;
+    phone: string;
+    imageUrl: string;
+    userAccNo: string;
+
+}
